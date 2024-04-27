fed855f3a8696c01899a77d21b7d0c47e2a0439f
lons <lonsnw@gmail.com>
2024-04-27 16:36:34 -0500

    Adding basic display functionality on Review; have not added database functionality

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,14 +1,18 @@
 import { useSelector } from 'react-redux';
+import { Card, CardContent, Button, Typography} from '@mui/material';
 
 function Review() {
     const feedback = useSelector(store => store.feedback);
 
     return(
-        <>
-        {feedback.map((entry, i) => 
-            <li key={i}> {entry}</li>
-        )}
-        </>
+        <Card>
+            <CardContent>
+                <Typography>How you're feeling: {feedback[0]}</Typography>
+                <Typography>How much you're understanding: {feedback[1]}</Typography>
+                <Typography>How supported you feel: {feedback[2]}</Typography>
+                <Typography>Your comments: {feedback[3]}</Typography>
+            </CardContent>
+        </Card>
     )
 }
 
